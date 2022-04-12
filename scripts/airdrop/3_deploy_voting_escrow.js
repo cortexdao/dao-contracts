fcf1ef6630566e476774124d8b82f439dfbfb318
Chan-Ho Suh <chanhosuh@gmail.com>
2022-04-12 17:12:10 -0400

    Create deploy script for voting escrow

---
+++ scripts/airdrop/3_deploy_voting_escrow.js
@@ -0,0 +1,98 @@
+#!/usr/bin/env node
+/*
+ * Command to run script:
+ *
+ * $ HARDHAT_NETWORK=<network name> node scripts/<script filename> --arg1=val1 --arg2=val2
+ */
+require("dotenv").config();
+const { argv } = require("yargs")
+  .option("compile", {
+    type: "boolean",
+    default: true,
+    description: "Compile contract using `compile:one`",
+  })
+  .option("maxFeePerGas", {
+    type: "number",
+    description: "Gas price in gwei; omitting uses default Ethers logic",
+  })
+  .option("maxPriorityFeePerGas", {
+    type: "number",
+    description: "Gas price in gwei; omitting uses default Ethers logic",
+  });
+const hre = require("hardhat");
+const { ethers, network } = require("hardhat");
+const { getMaxFee, getMaxPriorityFee } = require("../gas");
+
+const DAO_TOKEN_ADDRESS = "0x00";
+
+// eslint-disable-next-line no-unused-vars
+async function main(argv) {
+  await hre.run("clean");
+  await hre.run("compile");
+
+  const networkName = network.name.toUpperCase();
+  console.log("");
+  console.log(`${networkName} selected`);
+  console.log("");
+
+  if (!process.env.SAFE_OWNER_KEY) {
+    throw new Error("Must set SAFE_OWNER_KEY env var.");
+  }
+  const safeOwner = new ethers.Wallet(
+    process.env.SAFE_OWNER_KEY,
+    ethers.provider
+  );
+  console.log("Deployer: %s", safeOwner.address);
+  const balance =
+    (await ethers.provider.getBalance(safeOwner.address)).toString() / 1e18;
+  console.log("ETH balance (Safe owner): %s", balance);
+
+  const contractName = "DaoVotingEscrow";
+  console.log(`${contractName} deploy`);
+  console.log("");
+
+  const maxFeePerGas = await getMaxFee(argv.maxFeePerGas);
+  const maxPriorityFeePerGas = await getMaxPriorityFee(
+    argv.maxPriorityFeePerGas
+  );
+
+  console.log("Deploying ... ");
+  console.log("");
+
+  const contractFactory = await ethers.getContractFactory(contractName);
+  const contract = await contractFactory.connect(safeOwner).deploy(
+    DAO_TOKEN_ADDRESS, // token
+    "Vote-Locked CXD", // name
+    "vlCXD", // symbol
+    "1.0.0", // version
+    { maxFeePerGas, maxPriorityFeePerGas }
+  );
+  await contract.deployTransaction.wait(5);
+
+  console.log("Contract address: %s", contract.address);
+
+  console.log("Verify manually on the etherscan website.");
+  // can't verify vyper contracts through hardhat; must do through
+  // Etherscan website using this bytecode for the constructor args:
+  // (replace FFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFF with dao token address)
+  //
+  // vyper version 0.2.4
+  // 000000000000000000000000FFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFF000000000000000000000000000000000000000000000000000000000000008000000000000000000000000000000000000000000000000000000000000000c00000000000000000000000000000000000000000000000000000000000000100000000000000000000000000000000000000000000000000000000000000000f566f74652d4c6f636b65642043584400000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000005766c4358440000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000005312e302e30000000000000000000000000000000000000000000000000000000
+}
+
+if (!module.parent) {
+  main(argv)
+    .then(() => {
+      console.log("");
+      console.log("Contract deployed.");
+      console.log("");
+      process.exit(0);
+    })
+    .catch((error) => {
+      console.error(error);
+      console.log("");
+      process.exit(1);
+    });
+} else {
+  module.exports = main;
+}
